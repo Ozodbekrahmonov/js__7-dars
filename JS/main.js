866d3393cf5ce7bdca00d849fb45ad45b705de21
Ozodbek <ozodrahmonov2006@gmail.com>
2023-05-18 18:16:46 +0500

    js 7-dars
    Bu js 7-dars kodlari

---
+++ JS/main.js
@@ -0,0 +1,61 @@
+/* Matn uzunligi topish */
+/*let matn = "Assalomu alaykum shogirtlar!";
+console.log(matn.length);*/
+
+/* indexof (matn ichidan so'zning boshlanish indexini topish) */
+/*let matn = "sen gapir men esa sen bilan yozib boramiz";
+let birinchiSenIndex = matn.indexOf('sen')
+
+console.log(matn.indexOf('sen',birinchiSenIndex+1));*/
+/* replace(so'zlarni almashtirish qo'yish */
+let matn = "Salom hammaga! salom berdim hammaga! salom";
+/*console.log(matn.replace(/salom|hammaga|berdim/gi,'alik'));*/
+/*let yangiMatn = matn.replace(/salom|hammaga/gi,function (soz) {
+    if (soz == 'Salom' || soz == 'salom'){
+        return 'alik';
+    }  else if (soz == 'hammaga'){
+        return 'barchaga';
+    }
+});
+console.log(yangiMatn);*/
+
+/* Push & Pop (Arrayga ma'lumot kiritish va chiqarib olish)*/
+/*let list = [];
+list.push('Firemen');
+list.push('Freedom')
+list.push('Police')
+list.push('Olov Dalbayobov Musorovich');
+list.push('Saman');
+list.push("Ozod do'xtir")
+console.log(list)
+console.log(list.length);
+console.log(list.pop());
+console.log(list)
+console.log(list.pop());
+console.log(list)
+console.log(list.pop());
+console.log(list)*/
+
+/* Unshift & Shift (Arrayga ma'lumot kiritish va chiqarib olish) */
+/*let list = [];
+list.unshift('Olov ');
+list.unshift('Kamol');
+list.unshift('Bekzod')
+list.unshift('14')
+console.log(list);
+console.log(list.shift());
+console.log(list);
+console.log(list.shift());
+console.log(list);
+console.log(list.shift());
+console.log(list);*/
+
+/* sort (Tartibga keltirish) */
+/*let list = ['Kamal','OlovIT','Ozod','Begi','Muhammadamin','Shaxzod'];
+console.log(list.sort().reverse());*/
+/*let sonlar = [6,89,5,7,44,29,123,11];
+let tartiblanganSonlar = sonlar.sort(function (a,b){
+    return a-b;
+})
+console.log(tartiblanganSonlar.reverse())*/
+
